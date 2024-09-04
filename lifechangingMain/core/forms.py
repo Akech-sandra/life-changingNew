721eaf7d0d79cdfe612726b750f2bef5de0e0b04
Mmadiba724 <matendehussein@gmail.com>
2024-09-04 14:39:33 +0300

    backend custom users

--- lifechangingMain/core/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import *
-
-class T_donorForm(ModelForm):
-    class Meta:
-        model = T_donor
-        fields = '__all__'  
--- lifechangingMain/core/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-
-urlpatterns = [
-  path('donor/', views.donor, name='donor'),
-  ]
